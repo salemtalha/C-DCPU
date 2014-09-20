5c60694b283791a0e19e71c59140137f5bb134d3
Salem Talha <salem.a.talha@gmail.com>
2014-09-20 03:24:31 -0400

    some stubbing. still need to make architectural choices

--- emu.c
+++ emu.c
@@ -7,12 +7,10 @@
 typedef int16_t u16;
 
 u16 memory[0x10000] = { 0 };
-u16 registers[8] = { 0 };
-u16 pcounter = 0;
-u16 spointer = 0;
+u16 operands[12] = { 0 };
 
-enum regs {
-  A, B, C, X, Y, Z, I, J 
+enum operands {
+  A, B, C, X, Y, Z, I, J, PC, SP, EX, IA
 }; 
 
 enum ops {
@@ -23,8 +21,8 @@ enum ops {
 };
 
 void write_word(u16 value) {
-  memory[spointer] = value;
-  spointer++;
+  memory[operands[SP] = value];
+  operands[SP]++;
 }
 
 void dump_mem() {
@@ -38,4 +36,117 @@ void dump_mem() {
     }
     printf("%04x ", memory[i]);
   }
+}
+
+
+//u16 oper_values(u16 word) {
+//  if (word > 0x3f){
+//    printf("no good\n");
+//    exit(0);
+//  }
+//
+//  u16 next_word;
+//
+//  if (word <= 0x07)
+//    return word;
+//  if (word <= 0x0f)
+//    return operands[word - 0x08];
+//  if (word <= 0x17)
+//    fread(&next_word, 2, 1, stdin);
+//    return operands[operands[word] + next_word];
+//  if (word == 0x18)
+//    return 0;
+//  if (word == 0x19)
+//    return memory[operands[SP]];
+//  if (word == 0x1a)
+//    fread(&next_word, 2, 1, stdin);
+//    return memory[operands[SP]+ next_word];
+//  if (word == 0x1b)
+//    return operands[SP];
+//}
+
+void process(u16 word) {
+  u16 arg_o = (0x001f & word);
+  u16 arg_b = (0x03e0 & word) >> 5;
+  u16 arg_a = (0xfc00 & word) >> 10;
+
+
+  switch(arg_o) {
+    case SET:
+      break;
+    case ADD:
+      break;
+    case SUB:
+      break;
+    case MUL:
+      break;
+    case MLI:
+      break;
+    case DIV:
+      break;
+    case DVI:
+      break;
+    case MOD:
+      break;
+    case MDI:
+      break;
+    case AND:
+      break;
+    case BOR:
+      break;
+    case XOR:
+      break;
+    case SHR:
+      break;
+    case ASR:
+      break;
+    case SHL:
+      break;
+    case IFB:
+      break;
+    case IFC:
+      break;
+    case IFE:
+      break;
+    case IFN:
+      break;
+    case IFG:
+      break;
+    case IFA:
+      break;
+    case IFL:
+      break;
+    case IFU:
+      break;
+    case ADX:
+      break;
+    case SBX:
+      break;
+    case STI:
+      break;
+    case STD:
+      break;
+    default:
+      return;
+  }
+
+  printf("%04x %04x %04x\n", arg_a, arg_b, arg_o);  
+  exit(0);
+
+}
+
+void run() {
+
+}
+
+int main() {
+  u16 word = 0;
+
+  while(fread(&word, 2, 1, stdin)) {
+    //process(word);
+    write_word(word);
+  }
+
+  run();
+  //dump_mem();
 }
