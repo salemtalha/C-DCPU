fee08c36175c9a15dde088818607f5fb9a7b78d1
Salem Talha <salem.a.talha@gmail.com>
2014-09-20 18:16:20 -0400

    parsing for arguments, some work on operations

--- emu.c
+++ emu.c
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-typedef int16_t u16;
+typedef uint16_t u16;
 
 u16 memory[0x10000] = { 0 };
 u16 operands[12] = { 0 };
@@ -21,14 +21,24 @@ enum ops {
 };
 
 void write_word(u16 value) {
-  memory[operands[SP] = value];
-  operands[SP]++;
+  memory[operands[PC]] = value;
+  operands[PC]++;
 }
 
 void dump_mem() {
-  for(u16 i = 0; i < 10000; i++) {
+  printf("PC: %04x\tSP: %04x\tIA: %04x\tEX: %04x\n", operands[PC], operands[PC], operands[IA], operands[EX]);
+  printf("A: %04x\tB: %04x\tC: %04x\n", operands[A], operands[B], operands[C]);
+  printf("X: %04x\tY: %04x\tZ: %04x\n", operands[X], operands[Y], operands[Z]);
+  printf("I: %04x\tJ: %04x\n", operands[I], operands[J]);
+
+  printf("\n\n");
+
+  for(u16 i = 0; i <= 0xffff; i++) {
     if(i % 8 == 0 && i) {
       printf("\n");
+    }
+    if(i % 9 == 0) {
+      printf("%04x: ", i);
     }
     if (!memory[i]){
       printf("\n");
@@ -39,44 +49,100 @@ void dump_mem() {
 }
 
 
-//u16 oper_values(u16 word) {
-//  if (word > 0x3f){
-//    printf("no good\n");
-//    exit(0);
-//  }
-//
-//  u16 next_word;
-//
-//  if (word <= 0x07)
-//    return word;
-//  if (word <= 0x0f)
-//    return operands[word - 0x08];
-//  if (word <= 0x17)
-//    fread(&next_word, 2, 1, stdin);
-//    return operands[operands[word] + next_word];
-//  if (word == 0x18)
-//    return 0;
-//  if (word == 0x19)
-//    return memory[operands[SP]];
-//  if (word == 0x1a)
-//    fread(&next_word, 2, 1, stdin);
-//    return memory[operands[SP]+ next_word];
-//  if (word == 0x1b)
-//    return operands[SP];
-//}
+u16 *oper_values(u16 word, bool is_a) {
+  if (word > 0x3f){
+    printf("no good\n");
+    exit(0);
+  }
+
+  u16 next_word;
+
+  if (word <= 0x07)
+    return &(operands[word]);
+
+  if (word <= 0x0f)
+    return &(memory[operands[word - 0x08]]);
+
+  if (word <= 0x17) {
+    operands[PC]++;
+    next_word = memory[PC];
+    return &(memory[operands[word - 0x10] + next_word]);
+  }
+
+  if (word == 0x18) {
+    if (is_a)
+      return &(memory[--operands[SP]]);
+    else
+      return &(memory[operands[SP]++]);
+  }
+
+  if (word == 0x19)
+    return &(memory[operands[SP]]);
+
+  if (word == 0x1a) {
+    operands[PC]++;
+    next_word = memory[PC];
+    return &(memory[operands[SP]+ next_word]);
+  }
+
+  if (word == 0x1b)
+    return &(operands[SP]);
+
+  if (word == 0x1c)
+    return &(operands[PC]);
+
+  if (word == 0x1d)
+    return &(operands[EX]);
+
+  if (word == 0x1e) {
+    operands[PC]++;
+    next_word = memory[PC];
+    return &(memory[next_word]);
+  }
+
+  if (word == 0x1f) {
+    if (is_a) {
+      operands[PC]++;
+      next_word = memory[PC];
+      u16 *next_word_c = malloc(sizeof(u16));
+      next_word_c = &next_word;
+      return next_word_c;
+    }
+    return NULL;
+  }
+
+  if(word >= 0x20 && word <= 0x3f && is_a) {
+    word -= 0x21;
+    u16 *lit = malloc(sizeof(u16));
+    lit = &word;
+    return lit;
+  }
+
+}
 
 void process(u16 word) {
-  u16 arg_o = (0x001f & word);
-  u16 arg_b = (0x03e0 & word) >> 5;
-  u16 arg_a = (0xfc00 & word) >> 10;
+  u16 o = (0x001f & word);
+  u16 b = (0x03e0 & word) >> 5;
+  u16 a = (0xfc00 & word) >> 10;
 
+  u16 *oper1 = oper_values(b, false);
+  u16 *oper2 = oper_values(a, true);
 
-  switch(arg_o) {
+  switch(o) {
+    case NA:
+      break;
     case SET:
+      *oper1 = *oper2;
       break;
     case ADD:
+      *oper1 += *oper2;
+      if (*oper1 < *oper2)
+        operands[EX] = 0x0001;
+      else
+        operands[EX] = 0x0000;
       break;
     case SUB:
+      *oper1 -= *oper2;
       break;
     case MUL:
       break;
@@ -126,27 +192,27 @@ void process(u16 word) {
       break;
     case STD:
       break;
-    default:
-      return;
   }
 
-  printf("%04x %04x %04x\n", arg_a, arg_b, arg_o);  
-  exit(0);
-
+  operands[PC]++;
 }
 
-void run() {
 
+void run() {
+  while(operands[PC] != 0xffff) {
+    process(memory[operands[PC]]);
+  }
 }
 
 int main() {
   u16 word = 0;
-
   while(fread(&word, 2, 1, stdin)) {
-    //process(word);
     write_word(word);
   }
 
+  operands[SP] = 0xffff;
+  operands[PC] = 0;
+
   run();
-  //dump_mem();
+  dump_mem();
 }
